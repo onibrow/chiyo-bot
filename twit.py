557d7341ed7afd6b370870a6c938fc1538bf2904
onibrow <seiya.ono12@gmail.com>
2019-05-15 05:01:19 +0000

    remove old keys

--- twit.py
+++ twit.py
@@ -4,11 +4,6 @@ from auth_keys import twitter_keys
 
 twitter = Twython(twitter_keys[0], twitter_keys[1], twitter_keys[2], twitter_keys[3])
 
-cons_key = '2QYC8BipkGErbXI1wV4jUNMjV'
-cons_sec = 'V7PZksPzZxgUbGH2O3W8BkyjpZ8UcWfkkjZsGWSUDjrTlvCb7f'
-toke_key = '1128381792883363840-hvCWFrakC9ko2ZICl2gklQ1eIuvaft'
-toke_sec = 'm06Z4PaeP2RULvQ5X09KknmCUDhTB9QzKxt9RTTz0dWkd'
-
 def main():
     file_name = random.choice(os.listdir("img/"))
     file_path =  "img/" + file_name
