a6c54f3c394d7361dcceeac25ac670ec28481ef5
Seiya Ono <onibrow@users.noreply.github.com>
2019-05-14 22:06:19 -0700

    Update auth_keys.py

--- auth_keys.py
+++ auth_keys.py
@@ -1,8 +1,8 @@
 # YOUR KEYS HERE
 
-consumer_key = 'zdMZS3sX7xfXtPTvsEJhzKBWE'
-consumer_secret = '3Ohhocap95cVGE0gnRFyRc6y9J3WjMpBRNF1ctpuQUM4NR0XnK'
-token_key = '1128381792883363840-LgdBss3441if75h5LYPUJkYXc7OMfM'
-token_secret = 'foFqCS9PNo9kfciSHtLyWN7BaSBnmgJgVNcMgRFri9vaG'
+consumer_key = ''
+consumer_secret = ''
+token_key = ''
+token_secret = ''
 
 twitter_keys = [consumer_key, consumer_secret, token_key, token_secret]
