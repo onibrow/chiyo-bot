f6bc140288d368ad893f2f1c31888f26e8c09630
onibrow <seiya.ono12@gmail.com>
2019-05-25 05:54:15 +0000

    use script

--- twit.py
+++ twit.py
@@ -1,11 +1,11 @@
-import sys, os, random, git
+import sys, os, random, git, time
 from twython import Twython
 from auth_keys import twitter_keys
 
 twitter = Twython(twitter_keys[0], twitter_keys[1], twitter_keys[2], twitter_keys[3])
 repo = git.cmd.Git('.')
 
-def main():
+def post():
     repo.pull()
     file_name = random.choice(os.listdir("img/"))
     file_path =  "img/" + file_name
@@ -20,5 +20,11 @@ def main():
     twitter.update_status(status=message, media_ids=media_id)
     print("Tweeted: %s" % message)
 
+def main():
+    while (True):
+        post()
+        time.sleep(600)
+
 if __name__ == '__main__':
     main()
+
